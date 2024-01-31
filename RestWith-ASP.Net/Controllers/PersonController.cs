06a6c2f7eab37898ad6352009a34169f1ff9eed3
Daniel Lins Maciel <dan.8p@hotmail.com>
2024-01-31 20:00:36 -0300

    Creation of class Person

---
+++ RestWith-ASP.Net/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWith_ASP.Net.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RestWith-ASP.Net/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWith_ASP.Net.Model
+{
+    public class Person
+    {
+
+        public long id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
